17f3cac3be73aa61294beb4a58942c0be3ed2b0a
lxi <75556084+lxi1400@users.noreply.github.com>
2021-03-25 22:05:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 import requests
 import warnings, random
 warnings.filterwarnings("ignore")
+lol = ["True", "False"]
 
-def my_request():
-    url = "https://discord.com/api/v8/guilds/SERVERID/widget"
-    data = {'channel_id': RANDOMCHANNELID,
-            'enabled': True,
 
-           }
+def widget(guildid, token):
+    data = {'channel_id': "1",
+            'enabled': next(lol),
+    }
     headers = {
-        "Authorization": "TOKENHERE",
+        "Authorization": token,
         "Accept-Language": "en-US",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36",
         "Content-Type": "application/json",
         "Accept": "*/*",
     }
-    url2 = "https://discord.com/api/v8/guilds/SERVERID/widget"
-    data2 = {'channel_id': RANDOMCHANNELID,
-            'enabled': False,
 
-           }
-    headers2 = {
-        "Authorization": "TOKENHEREk",
-        "Accept-Language": "en-US",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36",
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-    }
-    r = requests.patch(url, headers=headers, json=data, verify=False)
-    r2 = requests.patch(url2, headers=headers2, json=data2, verify=False)
+    r = requests.patch(f"https://discord.com/api/v8/guilds/{guildid}/widget", headers=headers, json=data, verify=False)
+    print(r.status_code)
 
-for i in range(999999):
-    my_request()
+
+guildid = input("Insert Guild ID > ")
+token = input("Insert Token > ")
+
+while True:
+    widget(guildid, token)
